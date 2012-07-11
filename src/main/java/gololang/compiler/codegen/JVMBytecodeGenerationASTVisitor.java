3c0388386db688227347a3b7255700fd817cc75e
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-07-11 14:30:37 +0200

    Not sure the bytecode is correct, need to load to see...

--- src/main/java/gololang/compiler/codegen/JVMBytecodeGenerationASTVisitor.java
+++ src/main/java/gololang/compiler/codegen/JVMBytecodeGenerationASTVisitor.java
@@ -2,6 +2,7 @@ package gololang.compiler.codegen;
 
 import gololang.compiler.parser.*;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Type;
 
 import static org.objectweb.asm.Opcodes.*;
@@ -10,6 +11,12 @@ public class JVMBytecodeGenerationASTVisitor implements GoloParserVisitor {
 
   private final String goloSourceFilename;
   private final ClassWriter classWriter;
+
+  private static class Context {
+    String currentMethodName;
+    int currentMethodVisibility;
+    MethodVisitor currentMethodVisitor;
+  }
 
   public JVMBytecodeGenerationASTVisitor(String goloSourceFilename, ClassWriter classWriter) {
     this.goloSourceFilename = goloSourceFilename;
@@ -23,12 +30,12 @@ public class JVMBytecodeGenerationASTVisitor implements GoloParserVisitor {
 
   @Override
   public Object visit(SimpleNode node, Object data) {
-    return null;
+    throw new IllegalStateException();
   }
 
   @Override
   public Object visit(ASTCompilationUnit node, Object data) {
-    return node.childrenAccept(this, data);
+    return node.childrenAccept(this, new Context());
   }
 
   @Override
@@ -41,31 +48,62 @@ public class JVMBytecodeGenerationASTVisitor implements GoloParserVisitor {
 
   @Override
   public Object visit(ASTImportDeclaration node, Object data) {
-    return null;
+    return node.childrenAccept(this, data);
   }
 
   @Override
   public Object visit(ASTFunction node, Object data) {
+    Context context = (Context) data;
+    context.currentMethodVisitor = classWriter.visitMethod(
+        context.currentMethodVisibility | ACC_STATIC,
+        context.currentMethodName,
+        goloFunctionSignature(node),
+        null, null);
+    context.currentMethodVisitor.visitCode();
+    node.childrenAccept(this, data);
+    context.currentMethodVisitor.visitMaxs(0, 0);
+    context.currentMethodVisitor.visitEnd();
     return null;
+  }
+
+  private String goloFunctionSignature(ASTFunction node) {
+    StringBuilder descriptorBuilder = new StringBuilder("(");
+    for (int i = 0; i < node.getArguments().size(); i++) {
+      descriptorBuilder.append("Ljava/lang/Object;");
+    }
+    descriptorBuilder.append(")Ljava/lang/Object;");
+    return descriptorBuilder.toString();
   }
 
   @Override
   public Object visit(ASTFunctionDeclaration node, Object data) {
-    return null;
+    Context context = (Context) data;
+    context.currentMethodVisibility = node.isLocal() ? ACC_PRIVATE : ACC_PUBLIC;
+    context.currentMethodName = node.getName();
+    return node.childrenAccept(this, data);
   }
 
   @Override
   public Object visit(ASTLiteral node, Object data) {
+    Context context = (Context) data;
+    context.currentMethodVisitor.visitLdcInsn(node.getLiteralValue());
     return null;
   }
 
   @Override
   public Object visit(ASTReturnStatement node, Object data) {
+    Context context = (Context) data;
+    if (node.jjtGetNumChildren() > 0) {
+      node.childrenAccept(this, data);
+      context.currentMethodVisitor.visitInsn(ARETURN);
+    } else {
+      context.currentMethodVisitor.visitInsn(RETURN);
+    }
     return null;
   }
 
   @Override
   public Object visit(ASTBlock node, Object data) {
-    return null;
+    return node.childrenAccept(this, data);
   }
 }
--- src/test/resources/compiler-sanity-scripts/simple-returns.golo
+++ src/test/resources/compiler-sanity-scripts/simple-returns.golo
@@ -2,4 +2,6 @@ module golotest.SimpleReturns
 
 function nop = { }
 
+function voider = { return }
+
 function chuck = { return 42 }
