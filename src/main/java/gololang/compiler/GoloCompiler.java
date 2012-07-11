a367f6051bb612766473b7a613e19eb54eda5662
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-07-11 17:40:56 +0200

    Restyle.

--- src/main/java/gololang/compiler/GoloCompiler.java
+++ src/main/java/gololang/compiler/GoloCompiler.java
@@ -7,7 +7,10 @@ import gololang.compiler.parser.GoloParser;
 import gololang.compiler.parser.ParseException;
 import org.objectweb.asm.ClassWriter;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
 import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
@@ -26,13 +29,12 @@ public class GoloCompiler {
   }
 
   public CodeGenerationResult compileFromStream(String goloSourceFilename, InputStream sourceCodeInputStream) throws ParseException {
-
     ASTCompilationUnit compilationUnit = getParser(sourceCodeInputStream).CompilationUnit();
-
     JVMBytecodeGenerationASTVisitor bytecodeGenerationVisitor =
-        new JVMBytecodeGenerationASTVisitor(goloSourceFilename, new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS));
+        new JVMBytecodeGenerationASTVisitor(
+            goloSourceFilename,
+            new ClassWriter(COMPUTE_FRAMES | COMPUTE_MAXS));
     bytecodeGenerationVisitor.visit(compilationUnit, null);
-
     return new CodeGenerationResult(
         bytecodeGenerationVisitor.getBytecode(),
         bytecodeGenerationVisitor.getTargetJavaPackage(),
