4a2a3dece11e05ee3088701224d23fb338c098ea
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-07-18 14:14:18 +0200

    Cleanup

--- src/test/java/gololang/compiler/ParseTreeToGoloASTVisitorTest.java
+++ src/test/java/gololang/compiler/ParseTreeToGoloASTVisitorTest.java
@@ -1,6 +1,6 @@
 package gololang.compiler;
 
-import gololang.compiler.ast.*;
+import gololang.compiler.ast.GoloModule;
 import gololang.compiler.parser.ASTCompilationUnit;
 import gololang.compiler.parser.GoloParser;
 import gololang.compiler.parser.ParseException;
@@ -14,8 +14,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
@@ -40,43 +40,5 @@ public class ParseTreeToGoloASTVisitorTest {
     GoloModule module = visitor.transform(compilationUnit);
 
     assertThat(module, notNullValue());
-
-    // TODO: remove me
-    System.out.println("#########");
-    module.accept(new GoloASTVisitor() {
-      @Override
-      public void visitModule(GoloModule module) {
-        System.out.println("Module: " + module.getPackageAndClass() + ", " + module.getImports());
-        for (GoloFunction function : module.getFunctions().values()) {
-          function.accept(this);
-        }
-      }
-
-      @Override
-      public void visitFunction(GoloFunction function) {
-        System.out.println("Function: " + function.getName());
-        function.getBlock().accept(this);
-      }
-
-      @Override
-      public void visitBlock(GoloBlock block) {
-        System.out.println("Block: ");
-        for (GoloStatement statement : block.getStatements()) {
-          statement.accept(this);
-        }
-      }
-
-      @Override
-      public void visitConstantStatement(ConstantStatement constantStatement) {
-        System.out.println("Constant: " + constantStatement.getValue());
-      }
-
-      @Override
-      public void visitReturnStatement(ReturnStatement returnStatement) {
-        System.out.println("Return: ");
-        returnStatement.getExpressionStatement().accept(this);
-      }
-    });
-    System.out.println("#########");
   }
 }
