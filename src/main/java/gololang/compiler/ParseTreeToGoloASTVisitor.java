1d0398e992968627bb64e8164ab019a5000cbd7e
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-07-18 22:28:21 +0200

    Simplified the parse AST :-)

--- src/main/java/gololang/compiler/ParseTreeToGoloASTVisitor.java
+++ src/main/java/gololang/compiler/ParseTreeToGoloASTVisitor.java
@@ -139,10 +139,4 @@ class ParseTreeToGoloASTVisitor implements GoloParserVisitor {
     // TODO: to be implemented
     return node.childrenAccept(this, data);
   }
-
-  @Override
-  public Object visit(ASTExpressionStatement node, Object data) {
-    // TODO: to be implemented
-    return node.childrenAccept(this, data);
-  }
 }
