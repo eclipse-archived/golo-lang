31e902727c1e7181a56cc36849760bfd1d26c640
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-07-18 13:15:00 +0200

    Compile to a folder.

--- src/main/java/gololang/compiler/GoloCompiler.java
+++ src/main/java/gololang/compiler/GoloCompiler.java
@@ -5,6 +5,9 @@ import gololang.compiler.parser.ASTCompilationUnit;
 import gololang.compiler.parser.GoloParser;
 import gololang.compiler.parser.ParseException;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public final class GoloCompiler {
@@ -46,5 +49,20 @@ public final class GoloCompiler {
     JavaBytecodeGenerationGoloASTVisitor bytecodeGenerator = new JavaBytecodeGenerationGoloASTVisitor();
     byte[] bytes = bytecodeGenerator.toBytecode(goloModule, goloSourceFilename);
     return new Result(bytes, goloModule.getPackageAndClass());
+  }
+
+  public void compileTo(String goloSourceFilename, InputStream sourceCodeInputStream, File targetFolder) throws ParseException, IOException {
+    if (targetFolder.isFile()) {
+      throw new IllegalArgumentException(targetFolder + " already exists and is a file.");
+    }
+    Result result = compile(goloSourceFilename, sourceCodeInputStream);
+    File outputFolder = new File(targetFolder, result.packageAndClass.packageName().replaceAll("\\.", File.pathSeparator));
+    if (!outputFolder.mkdirs()) {
+      throw new IOException("mkdir() failed on " + outputFolder);
+    }
+    File outputFile = new File(outputFolder, result.packageAndClass.className() + ".class");
+    try (FileOutputStream out = new FileOutputStream(outputFile)) {
+      out.write(result.bytecode);
+    }
   }
 }
