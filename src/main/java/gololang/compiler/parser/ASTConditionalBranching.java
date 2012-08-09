9265d17bf1ba00a40afb894423c33324b46b2c14
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-08-09 23:31:53 +0200

    ASTConditionalBranching.java

---
+++ src/main/java/gololang/compiler/parser/ASTConditionalBranching.java
@@ -0,0 +1,22 @@
+package gololang.compiler.parser;
+
+public class ASTConditionalBranching extends GoloASTNode {
+
+  public ASTConditionalBranching(int id) {
+    super(id);
+  }
+
+  public ASTConditionalBranching(GoloParser p, int id) {
+    super(p, id);
+  }
+
+  @Override
+  public String toString() {
+    return "ASTConditionalBranching{}";
+  }
+
+  @Override
+  public Object jjtAccept(GoloParserVisitor visitor, Object data) {
+    return visitor.visit(this, data);
+  }
+}
