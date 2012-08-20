608efd619a00f6e961728b025f617f8c426458f1
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-08-20 18:56:55 +0200

    New operators symbols.

--- src/main/java/gololang/runtime/BinaryOperationType.java
+++ src/main/java/gololang/runtime/BinaryOperationType.java
@@ -2,7 +2,18 @@ package gololang.runtime;
 
 public enum BinaryOperationType {
 
-  PLUS("+"), MINUS("-"), TIMES("*"), DIVIDE("/");
+  PLUS("+"),
+  MINUS("-"),
+  TIMES("*"),
+  DIVIDE("/"),
+
+  EQUALS("=="),
+  NOTEQUALS("!="),
+  LESS("<"),
+  LESSOREQUALS("<="),
+  MORE(">"),
+  MOREOREQUALS(">=")
+  ;
 
   private final String symbol;
 
