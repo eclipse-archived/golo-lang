7abe9a02bfaf60bc8afcb77c9830b57725f3085d
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-10-15 15:02:42 +0200

    Moved back, and check that only static methods are being looked at

--- src/main/java/fr/insalyon/citi/golo/runtime/BootstrapHelpers.java
+++ src/main/java/fr/insalyon/citi/golo/runtime/BootstrapHelpers.java
@@ -24,25 +24,4 @@ class BootstrapHelpers {
     }
     return false;
   }
-
-  static Object findStaticMethodOrField(Class<?> klass, String name, Class<?>[] argumentTypes) {
-    for (Method method : klass.getDeclaredMethods()) {
-      if (method.getName().equals(name)) {
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        if (parameterTypes.length == argumentTypes.length) {
-          return method;
-        } else if (method.isVarArgs() && (argumentTypes.length >= parameterTypes.length)) {
-          return method;
-        }
-      }
-    }
-    if (argumentTypes.length == 0) {
-      for (Field field : klass.getDeclaredFields()) {
-        if (field.getName().equals(name) && isStatic(field.getModifiers())) {
-          return field;
-        }
-      }
-    }
-    return null;
-  }
 }
