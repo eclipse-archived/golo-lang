6167c157b8bfe0c20e2e6f24fe5fc0366b82671f
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-11-10 00:03:26 +0100

    Adding a loop to increase the test call site megamorphism level

--- src/test/java/fr/insalyon/citi/golo/runtime/MethodInvocationSupportTest.java
+++ src/test/java/fr/insalyon/citi/golo/runtime/MethodInvocationSupportTest.java
@@ -1,13 +1,9 @@
 package fr.insalyon.citi.golo.runtime;
 
-import org.objectweb.asm.tree.MethodNode;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.lang.invoke.CallSite;
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
@@ -110,31 +106,33 @@ public class MethodInvocationSupportTest {
     CallSite toString = MethodInvocationSupport.bootstrap(lookup(), "toString", methodType(Object.class, Object.class));
     MethodHandle toStringMH = toString.dynamicInvoker();
 
-    String result = (String) toStringMH.invokeWithArguments(julien());
-    assertThat(result, is("Person{name='Julien', email='julien.ponge@insa-lyon.fr'}"));
+    for (int i = 0; i < 5; i++) {
+      String result = (String) toStringMH.invokeWithArguments(julien());
+      assertThat(result, is("Person{name='Julien', email='julien.ponge@insa-lyon.fr'}"));
 
-    result = (String) toStringMH.invokeWithArguments("foo");
-    assertThat(result, is("foo"));
+      result = (String) toStringMH.invokeWithArguments("foo");
+      assertThat(result, is("foo"));
 
-    result = (String) toStringMH.invokeWithArguments(666);
-    assertThat(result, is("666"));
+      result = (String) toStringMH.invokeWithArguments(666);
+      assertThat(result, is("666"));
 
-    result = (String) toStringMH.invokeWithArguments(666L);
-    assertThat(result, is("666"));
+      result = (String) toStringMH.invokeWithArguments(666L);
+      assertThat(result, is("666"));
 
-    result = (String) toStringMH.invokeWithArguments("foo");
-    assertThat(result, is("foo"));
+      result = (String) toStringMH.invokeWithArguments("foo");
+      assertThat(result, is("foo"));
 
-    result = (String) toStringMH.invokeWithArguments(new BigInteger("1234"));
-    assertThat(result, is("1234"));
+      result = (String) toStringMH.invokeWithArguments(new BigInteger("1234"));
+      assertThat(result, is("1234"));
 
-    result = (String) toStringMH.invokeWithArguments(new Object() {
-      @Override
-      public String toString() {
-        return "Hey!";
-      }
-    });
-    assertThat(result, is("Hey!"));
+      result = (String) toStringMH.invokeWithArguments(new Object() {
+        @Override
+        public String toString() {
+          return "Hey!";
+        }
+      });
+      assertThat(result, is("Hey!"));
+    }
   }
 
   @Test
