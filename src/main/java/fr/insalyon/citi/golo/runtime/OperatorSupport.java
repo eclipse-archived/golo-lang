b939479207d34d80b0c35632462795de94a0fa43
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-11-13 19:40:40 +0100

    Switching to inline cached operators
    (+) passes the tests

--- src/main/java/fr/insalyon/citi/golo/runtime/OperatorSupport.java
+++ src/main/java/fr/insalyon/citi/golo/runtime/OperatorSupport.java
@@ -2,18 +2,137 @@ package fr.insalyon.citi.golo.runtime;
 
 import java.lang.invoke.*;
 
+import static java.lang.invoke.MethodHandles.guardWithTest;
+import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodHandles.lookup;
 import static java.lang.invoke.MethodType.methodType;
 
 public class OperatorSupport {
 
-  public static CallSite bootstrap(MethodHandles.Lookup caller, String name, MethodType type, int arity) throws NoSuchMethodException, IllegalAccessException {
-    MethodHandle handle;
-    if (arity == 1) {
-      handle = caller.findStatic(OperatorSupport.class, name, methodType(Object.class, Object.class));
-    } else {
-      handle = caller.findStatic(OperatorSupport.class, name, methodType(Object.class, Object.class, Object.class));
+  static class InlineCache extends MutableCallSite {
+
+    final MethodHandles.Lookup callerLookup;
+    final String name;
+
+    InlineCache(MethodHandles.Lookup callerLookup, String name, MethodType type) {
+      super(type);
+      this.callerLookup = callerLookup;
+      this.name = name;
     }
-    return new ConstantCallSite(handle);
+  }
+
+  private static final MethodHandle GUARD_2;
+  private static final MethodHandle FALLBACK_2;
+
+  static {
+    try {
+      MethodHandles.Lookup lookup = MethodHandles.lookup();
+
+      GUARD_2 = lookup.findStatic(
+          OperatorSupport.class,
+          "guard_2",
+          methodType(boolean.class, Class.class, Class.class, Object.class, Object.class));
+
+      FALLBACK_2 = lookup.findStatic(
+          OperatorSupport.class,
+          "fallback_2",
+          methodType(Object.class, InlineCache.class, Object[].class));
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      throw new Error("Could not bootstrap the required method handles", e);
+    }
+  }
+
+  public static boolean guard_2(Class<?> expected1, Class<?> expected2, Object receiver1, Object receiver2) {
+    return (receiver1.getClass() == expected1) && (receiver2.getClass() == expected2);
+  }
+
+  public static Object fallback_2(InlineCache inlineCache, Object[] args) throws Throwable {
+
+    Class<?> receiverClass1 = args[0].getClass();
+    Class<?> receiverClass2 = args[1].getClass();
+    MethodHandle target;
+
+    try {
+      target = inlineCache.callerLookup.findStatic(
+          OperatorSupport.class, inlineCache.name, methodType(Object.class, receiverClass1, receiverClass2));
+    } catch (Throwable t1) {
+      try {
+        target = inlineCache.callerLookup.findStatic(
+            OperatorSupport.class, inlineCache.name + "_fallback", methodType(Object.class, Object.class, Object.class));
+      } catch (Throwable t2) {
+        return reject(args[0], args[1], inlineCache.name);
+      }
+    }
+    target = target.asType(methodType(Object.class, Object.class, Object.class));
+
+    MethodHandle guard = insertArguments(GUARD_2, 0, receiverClass1, receiverClass2);
+
+    MethodHandle guardedTarget = guardWithTest(guard, target, inlineCache.getTarget());
+    inlineCache.setTarget(guardedTarget);
+    return target.invokeWithArguments(args);
+  }
+
+  public static CallSite bootstrap(MethodHandles.Lookup caller, String name, MethodType type, int arity) throws NoSuchMethodException, IllegalAccessException {
+    InlineCache callSite = new InlineCache(caller, name, type);
+    MethodHandle fallbackHandle;
+    if (arity == 2) {
+      fallbackHandle = FALLBACK_2
+          .bindTo(callSite)
+          .asCollector(Object[].class, type.parameterCount())
+          .asType(type);
+    } else {
+      throw new UnsupportedOperationException("no fallback for unary operators yet");
+    }
+    callSite.setTarget(fallbackHandle);
+    return callSite;
+  }
+
+  public static Object plus(Integer a, Integer b) {
+    return a + b;
+  }
+
+  public static Object plus(Long a, Long b) {
+    return a + b;
+  }
+
+  public static Object plus(Integer a, Long b) {
+    return ((long) a) + b;
+  }
+
+  public static Object plus(Long a, Integer b) {
+    return a + ((long) b);
+  }
+
+  public static Object plus(String a, String b) {
+    return a + b;
+  }
+
+  public static Object plus_fallback(Object a, Object b) {
+    if (isNotNullAndString(a) || isNotNullAndString(b)) {
+      return new StringBuilder().append(a).append(b).toString();
+    }
+    return reject(a, b, "plus");
+  }
+
+  public static Object plus(Object a, Object b) {
+    if (bothNotNull(a, b)) {
+      if (isInteger(a) && isInteger(b)) {
+        return (Integer) a + (Integer) b;
+      }
+      if (isLong(a) && isLong(b)) {
+        return (Long) a + (Long) b;
+      }
+      if (isLong(a) && isInteger(b)) {
+        return (Long) a + (long) ((Integer) b);
+      }
+      if (isInteger(a) && isLong(b)) {
+        return (long) ((Integer) a) + (Long) b;
+      }
+    }
+    if (isNotNullAndString(a) || isNotNullAndString(b)) {
+      return new StringBuilder().append(a).append(b).toString();
+    }
+    return reject(a, b, "+");
   }
 
   private static boolean bothNotNull(Object a, Object b) {
@@ -46,27 +165,6 @@ public class OperatorSupport {
 
   private static boolean isClass(Object obj) {
     return (obj != null) && (obj.getClass() == Class.class);
-  }
-
-  public static Object plus(Object a, Object b) {
-    if (bothNotNull(a, b)) {
-      if (isInteger(a) && isInteger(b)) {
-        return (Integer) a + (Integer) b;
-      }
-      if (isLong(a) && isLong(b)) {
-        return (Long) a + (Long) b;
-      }
-      if (isLong(a) && isInteger(b)) {
-        return (Long) a + (long) ((Integer) b);
-      }
-      if (isInteger(a) && isLong(b)) {
-        return (long) ((Integer) a) + (Long) b;
-      }
-    }
-    if (isNotNullAndString(a) || isNotNullAndString(b)) {
-      return new StringBuilder().append(a).append(b).toString();
-    }
-    return reject(a, b, "+");
   }
 
   public static Object minus(Object a, Object b) {
