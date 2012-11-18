18561e0b9bf9cd6da9304e2f12428f5f32d563a8
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-11-18 12:44:15 +0100

    Improved HTTP benchmarks server with an /index URL

--- benchmarks/http_server.rb
+++ benchmarks/http_server.rb
@@ -1,10 +1,48 @@
 #!/usr/bin/env ruby
+#
+require 'erb'
 require 'webrick'
 
- server = WEBrick::HTTPServer.new(:Port => 1981)
- server.mount "/", WEBrick::HTTPServlet::FileHandler, './'
- trap "INT" do 
-   server.shutdown
- end
- server.start
+server = WEBrick::HTTPServer.new(:Port => 1981)
+
+index_template = ERB.new <<-EOF
+<html lang="en">
+
+<head>
+  <title>Benchmarks</title>
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" 
+        rel="stylesheet">
+  <style>
+    body { padding-top: 60px; }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <h1>Benchmarks</h1>
+    <ul>
+      <%= @links %>
+    </ul>
+  </div>
+</body>
+
+</html>
+EOF
+
+server.mount_proc('/index') do |request, response| 
+  @links = ""
+  Dir['*.html'].each do |filename|
+    @links = @links + "<li><a href=\"#{filename}\">#{filename}</a></li>\n"
+  end
+  response['Content-Type'] = 'text/html'
+  response.body = index_template.result
+end
+
+server.mount "/", WEBrick::HTTPServlet::FileHandler, './'
+ 
+trap "INT" do 
+  server.shutdown
+end
+ 
+server.start
 
