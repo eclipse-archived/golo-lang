c0e2af106d59624266a962a1b45885809748b778
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-11-21 18:25:43 +0100

    JRuby polymorphic test

--- benchmarks/src/test/java/dispatch/PolymorphicTest.java
+++ benchmarks/src/test/java/dispatch/PolymorphicTest.java
@@ -6,6 +6,8 @@ import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
 import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
 import com.carrotsearch.junitbenchmarks.annotation.LabelType;
 import fr.citilab.gololang.benchmarks.GoloBenchmark;
+import org.jruby.embed.EmbedEvalUnit;
+import org.jruby.embed.ScriptingContainer;
 import org.junit.Test;
 
 @BenchmarkOptions(clock = Clock.NANO_TIME, warmupRounds = 10)
@@ -15,6 +17,13 @@ public class PolymorphicTest extends GoloBenchmark {
 
   private static final Class<?> GoloModule = loadGoloModule("Polymorphic.golo");
   private static final Class<?> GroovyClass = loadGroovyClass("Polymorphic.groovy");
+  private static final ScriptingContainer JRubyContainer;
+  private static final EmbedEvalUnit JRubyScript;
+
+  static {
+    JRubyContainer = new ScriptingContainer();
+    JRubyScript = jrubyEvalUnit(JRubyContainer, "polymorphic.rb");
+  }
 
   @Test
   public void java() {
@@ -29,5 +38,10 @@ public class PolymorphicTest extends GoloBenchmark {
   @Test
   public void groovy() throws Throwable {
     GroovyClass.getMethod("run").invoke(null);
+  }
+
+  @Test
+  public void jruby() throws Throwable {
+    JRubyScript.run();
   }
 }
