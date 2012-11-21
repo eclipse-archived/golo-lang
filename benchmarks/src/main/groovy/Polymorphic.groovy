a2c738ca96f220ffbb75abacbfe6a170a432c256
Julien Ponge <julien.ponge@insa-lyon.fr>
2012-11-21 18:10:50 +0100

    Groovy polymorphic test

---
+++ benchmarks/src/main/groovy/Polymorphic.groovy
@@ -0,0 +1,32 @@
+import java.util.HashMap
+import java.util.LinkedList
+import java.util.TreeSet
+
+class Polymorphic {
+
+    static Object run() {
+        Object[] data = [
+            "foo",
+            666,
+            new Object(),
+            "bar",
+            999,
+            new LinkedList<>(),
+            new HashMap<>(),
+            new TreeSet<>(),
+            new RuntimeException(),
+            new IllegalArgumentException(),
+            new IllegalStateException(),
+            new Object(),
+            new Exception()
+        ]
+        Object result = null
+        for (int i = 0; i < 1000000; i++) {
+            for (int j = 0; j < data.length; j++) {
+                result = data[j].toString()
+            }
+        }
+        return result
+    }
+}
+
--- benchmarks/src/test/java/dispatch/PolymorphicTest.java
+++ benchmarks/src/test/java/dispatch/PolymorphicTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 public class PolymorphicTest extends GoloBenchmark {
 
   private static final Class<?> GoloModule = loadGoloModule("Polymorphic.golo");
+  private static final Class<?> GroovyClass = loadGroovyClass("Polymorphic.groovy");
 
   @Test
   public void java() {
@@ -23,5 +24,10 @@ public class PolymorphicTest extends GoloBenchmark {
   @Test
   public void golo() throws Throwable {
     GoloModule.getMethod("run").invoke(null);
+  }
+
+  @Test
+  public void groovy() throws Throwable {
+    GroovyClass.getMethod("run").invoke(null);
   }
 }
