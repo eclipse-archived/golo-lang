eb89a9f8bb5003a1e11af191831ca838e4772d9e
Julien Ponge <julien.ponge@insa-lyon.fr>
2013-02-05 14:03:29 +0100

    Test cases for the pimps on j.u.Set

--- src/test/java/gololang/StandardPimpsTest.java
+++ src/test/java/gololang/StandardPimpsTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import static fr.insalyon.citi.golo.internal.testing.TestUtils.compileAndLoadGoloModule;
@@ -66,6 +67,64 @@ public class StandardPimpsTest {
   public void lists_each() throws Throwable {
     Method lists_each = moduleClass.getMethod("lists_each");
     Object result = lists_each.invoke(null);
+    assertThat(result, instanceOf(Integer.class));
+    assertThat((Integer) result, is(10));
+  }
+
+  @Test
+  public void sets_has_single() throws Throwable {
+    Method sets_has_single = moduleClass.getMethod("sets_has_single");
+    assertThat((Boolean) sets_has_single.invoke(null), is(true));
+  }
+
+  @Test
+  public void sets_has_single_not() throws Throwable {
+    Method sets_has_single_not = moduleClass.getMethod("sets_has_single_not");
+    assertThat((Boolean) sets_has_single_not.invoke(null), is(false));
+  }
+
+  @Test
+  public void sets_has_many() throws Throwable {
+    Method sets_has_many = moduleClass.getMethod("sets_has_many");
+    assertThat((Boolean) sets_has_many.invoke(null), is(true));
+  }
+
+  @Test
+  public void sets_has_many_not() throws Throwable {
+    Method sets_has_many_not = moduleClass.getMethod("sets_has_many_not");
+    assertThat((Boolean) sets_has_many_not.invoke(null), is(false));
+  }
+
+  @Test
+  public void sets_filter() throws Throwable {
+    Method sets_filter = moduleClass.getMethod("sets_filter");
+    Object result = sets_filter.invoke(null);
+    assertThat(result, instanceOf(Set.class));
+    Set<Integer> set = (Set<Integer>) result;
+    assertThat(set, containsInAnyOrder(0, 2, 4));
+  }
+
+  @Test
+  public void sets_map() throws Throwable {
+    Method sets_map = moduleClass.getMethod("sets_map");
+    Object result = sets_map.invoke(null);
+    assertThat(result, instanceOf(Set.class));
+    Set<Integer> set = (Set<Integer>) result;
+    assertThat(set, containsInAnyOrder(0, 10, 20, 30, 40));
+  }
+
+  @Test
+  public void sets_reduce() throws Throwable {
+    Method sets_reduce = moduleClass.getMethod("sets_reduce");
+    Object result = sets_reduce.invoke(null);
+    assertThat(result, instanceOf(Integer.class));
+    assertThat((Integer) result, is(10));
+  }
+
+  @Test
+  public void sets_each() throws Throwable {
+    Method sets_each = moduleClass.getMethod("sets_each");
+    Object result = sets_each.invoke(null);
     assertThat(result, instanceOf(Integer.class));
     assertThat((Integer) result, is(10));
   }
--- src/test/resources/for-test/bootstrapped-standard-pimps.golo
+++ src/test/resources/for-test/bootstrapped-standard-pimps.golo
@@ -27,3 +27,27 @@ function lists_each = {
   list_data(): each(|v| -> int: addAndGet(v))
   return int: get()
 }
+
+local function set_data = {
+  return HashSet():
+    include(0):
+    include(1, 2, 3, 4, 5, 6, 7):
+    exclude(7):
+    exclude(5, 6)
+}
+
+function sets_has_single = -> set_data(): has(2)
+function sets_has_single_not = -> set_data(): has(10)
+
+function sets_has_many = -> set_data(): has(2, 3)
+function sets_has_many_not = -> set_data(): has(2, 30)
+
+function sets_filter = -> set_data(): filter(|v| -> (v % 2) is 0): unmodifiableView()
+function sets_map = -> set_data(): map(|v| -> v * 10): unmodifiableView()
+function sets_reduce = -> set_data(): reduce(0, |acc, v| -> acc + v)
+
+function sets_each = {
+  let int = AtomicInteger(0)
+  set_data(): each(|v| -> int: addAndGet(v))
+  return int: get()
+}
