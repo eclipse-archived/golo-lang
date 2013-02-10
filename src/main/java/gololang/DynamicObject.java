8cc952046833f3cea2bf935888facd28ed9bebed
Julien Ponge <julien.ponge@insa-lyon.fr>
2013-02-10 19:35:30 +0100

    Towards dynamic objects

---
+++ src/main/java/gololang/DynamicObject.java
@@ -0,0 +1,94 @@
+package gololang;
+
+import java.lang.invoke.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.invoke.MethodHandles.dropArguments;
+import static java.lang.invoke.MethodHandles.insertArguments;
+import static java.lang.invoke.MethodType.methodType;
+
+public class DynamicObject {
+
+  private final Map<String, Set<SwitchPoint>> switchPoints = new HashMap<>();
+  private final Map<String, Object> properties = new HashMap<>();
+
+  public DynamicObject define(String name, Object value) {
+    properties.put(name, value);
+    if (switchPoints.containsKey(name)) {
+      invalidate(name);
+    } else {
+      switchPoints.put(name, new HashSet<SwitchPoint>());
+    }
+    return this;
+  }
+
+  private void invalidate(String name) {
+    Set<SwitchPoint> switches = switchPoints.get(name);
+    SwitchPoint.invalidateAll(switches.toArray(new SwitchPoint[switches.size()]));
+    switches.clear();
+  }
+
+  public Object get(String name) {
+    return properties.get(name);
+  }
+
+  public DynamicObject undefine(String name) {
+    if (properties.containsKey(name)) {
+      properties.remove(name);
+      invalidate(name);
+      switchPoints.remove(name);
+    }
+    return this;
+  }
+
+  private static final MethodHandle FALLBACK;
+  private static final MethodHandle PROPERTY_MISSING;
+
+  static {
+    MethodHandles.Lookup lookup = MethodHandles.lookup();
+    try {
+      FALLBACK = lookup.findStatic(DynamicObject.class, "fallback",
+          methodType(Object.class, MutableCallSite.class, String.class, DynamicObject.class, Object[].class));
+      PROPERTY_MISSING = lookup.findStatic(DynamicObject.class, "propertyMissing",
+          methodType(Object.class, String.class));
+    } catch (NoSuchMethodException | IllegalAccessException e) {
+      throw new Error("Could not bootstrap the required method handles");
+    }
+  }
+
+  public static Object fallback(MutableCallSite callSite, String name, DynamicObject dynamicObject, Object[] args) throws Throwable {
+    dynamicObject.plug(callSite, name);
+    return callSite.dynamicInvoker().invokeWithArguments(args);
+  }
+
+  public static Object propertyMissing(String name) throws NoSuchMethodException {
+    throw new NoSuchMethodException("Missing DynamicObject definition for " + name);
+  }
+
+  public MutableCallSite plug(MutableCallSite callSite, String name) {
+    MethodHandle target;
+    Object value = properties.get(name);
+    boolean isFunction = value instanceof MethodHandle;
+    if (value != null) {
+      if (isFunction) {
+        target = (MethodHandle) value;
+      } else {
+        target = MethodHandles.constant(Object.class, value);
+      }
+    } else {
+      target = PROPERTY_MISSING.bindTo(name);
+      switchPoints.put(name, new HashSet<SwitchPoint>());
+    }
+    MethodType type = callSite.type();
+    MethodHandle fallback = insertArguments(FALLBACK, 0, callSite, name, this)
+        .asCollector(Object[].class, type.parameterCount())
+        .asType(type);
+    SwitchPoint switchPoint = new SwitchPoint();
+    callSite.setTarget(switchPoint.guardWithTest(target.asType(type), fallback));
+    switchPoints.get(name).add(switchPoint);
+    return callSite;
+  }
+}
---
+++ src/test/java/gololang/DynamicObjectTest.java
@@ -0,0 +1,57 @@
+package gololang;
+
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.TestException;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
+
+import static java.lang.invoke.MethodType.genericMethodType;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.fail;
+
+public class DynamicObjectTest {
+
+  @Test
+  public void define_undefine_value() {
+    DynamicObject dynamicObject = new DynamicObject();
+    assertThat(dynamicObject.get("name"), nullValue());
+
+    dynamicObject.define("name", "Mr Bean");
+    assertThat((String) dynamicObject.get("name"), is("Mr Bean"));
+
+    dynamicObject.undefine("name");
+    assertThat(dynamicObject.get("name"), nullValue());
+  }
+
+  @Test
+  public void plug_value() throws Throwable {
+    DynamicObject dynamicObject = new DynamicObject();
+    dynamicObject.define("name", "Mr Bean");
+
+    MutableCallSite callSite = new MutableCallSite(genericMethodType(0));
+    dynamicObject.plug(callSite, "name");
+    MethodHandle invoker = callSite.dynamicInvoker();
+    Object result = invoker.invoke();
+    assertThat(result, instanceOf(String.class));
+    assertThat((String) result, is("Mr Bean"));
+
+    dynamicObject.undefine("name");
+    assertThat(dynamicObject.get("name"), nullValue());
+    try {
+      invoker.invoke();
+      fail("Expected NoSuchMethodException");
+    } catch (NoSuchMethodException expected) {
+    }
+
+    dynamicObject.define("name", "John B Root");
+    assertThat((String) invoker.invoke(), is("John B Root"));
+
+    dynamicObject.define("name", "John B Rootz");
+    assertThat((String) invoker.invoke(), is("John B Rootz"));
+  }
+}
