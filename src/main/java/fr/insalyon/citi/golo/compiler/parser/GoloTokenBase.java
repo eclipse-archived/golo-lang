05f141c0f99f883c464a1f015c03e58f42f20dce
David Festal <david.festal@serli.com>
2013-03-06 14:49:09 +0100

    Create a Token superclass implementation to add offsets and previous token

---
+++ src/main/java/fr/insalyon/citi/golo/compiler/parser/GoloTokenBase.java
@@ -0,0 +1,11 @@
+package fr.insalyon.citi.golo.compiler.parser;
+
+/**
+ *
+ * @author david
+ */
+public class GoloTokenBase {
+    public int startOffset;
+    public int endOffset;
+    public Token previousToken;
+}
