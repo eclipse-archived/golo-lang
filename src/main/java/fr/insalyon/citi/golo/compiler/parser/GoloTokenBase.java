68a5676efc2045ec5ba42ce14addada3e6e08eba
Julien Ponge <julien.ponge@insa-lyon.fr>
2013-03-07 10:04:50 +0100

    Format / restyle

--- src/main/java/fr/insalyon/citi/golo/compiler/parser/GoloTokenBase.java
+++ src/main/java/fr/insalyon/citi/golo/compiler/parser/GoloTokenBase.java
@@ -1,11 +1,7 @@
 package fr.insalyon.citi.golo.compiler.parser;
 
-/**
- *
- * @author david
- */
 public class GoloTokenBase {
-    public int startOffset;
-    public int endOffset;
-    public Token previousToken;
+  public int startOffset;
+  public int endOffset;
+  public Token previousToken;
 }
