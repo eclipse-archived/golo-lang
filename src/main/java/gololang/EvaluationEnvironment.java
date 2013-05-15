cfe9c071987dfae8f0f68e6ac0241451866cbf45
Julien Ponge <julien.ponge@insa-lyon.fr>
2013-05-15 23:27:18 +0200

    Evaluation environment for dynamic code loading

---
+++ src/main/java/gololang/EvaluationEnvironment.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012-2013 Institut National des Sciences Appliquées de Lyon (INSA-Lyon)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gololang;
+
+import fr.insalyon.citi.golo.compiler.GoloClassLoader;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+
+public class EvaluationEnvironment {
+
+  private final GoloClassLoader goloClassLoader = new GoloClassLoader();
+
+  private static String anonymousFilename() {
+    return "$Anonymous$_" + System.nanoTime() + ".golo";
+  }
+
+  private static String anonymousModuleName() {
+    return "module anonymous" + System.nanoTime();
+  }
+
+  public Object module(String source) {
+    try (InputStream in = new ByteArrayInputStream(source.getBytes())) {
+      return goloClassLoader.load(anonymousFilename(), in);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public Object anonymousModule(String source) {
+    return module(anonymousModuleName() + "\n\n" + source);
+  }
+
+  public Object function(String source) {
+    return loadAndRun(source, "$_code_ref");
+  }
+  public Object run(String source) {
+    return loadAndRun(source, "$_code");
+  }
+
+  private Class<?> wrapAndLoad(String source) {
+    StringBuilder builder = new StringBuilder()
+        .append(anonymousModuleName())
+        .append("\n\n")
+        .append("function $_code = {\n")
+        .append(source)
+        .append("\n}\n\n")
+        .append("function $_code_ref = -> ^$_code\n\n");
+    return (Class<?>) module(builder.toString());
+  }
+
+  private Object loadAndRun(String source, String target) {
+    try {
+      Class<?> module = wrapAndLoad(source);
+      return module.getMethod(target).invoke(null);
+    } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/test/java/gololang/EvaluationEnvironmentTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2012-2013 Institut National des Sciences Appliquées de Lyon (INSA-Lyon)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gololang;
+
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandle;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class EvaluationEnvironmentTest {
+
+  private static final String SIMPLE_MODULE =
+      "module Foo\n\n" +
+          "function plop = -> \"Plop!\"\n\n";
+
+  private static final String SIMPLE_ANONYMOUS_MODULE = "function plop = -> \"Plop!\"\n\n";
+
+  @Test
+  public void module() throws Throwable {
+    EvaluationEnvironment env = new EvaluationEnvironment();
+    Class<?> module = (Class<?>) env.module(SIMPLE_MODULE);
+
+    assertThat(module, not(nullValue()));
+    assertThat((String) module.getMethod("plop").invoke(null), is("Plop!"));
+  }
+
+  @Test
+  public void anonymous_module() throws Throwable {
+    EvaluationEnvironment env = new EvaluationEnvironment();
+    Class<?> module = (Class<?>) env.anonymousModule(SIMPLE_ANONYMOUS_MODULE);
+
+    assertThat(module, not(nullValue()));
+    assertThat((String) module.getMethod("plop").invoke(null), is("Plop!"));
+  }
+
+  @Test
+  public void function() throws Throwable {
+    EvaluationEnvironment env = new EvaluationEnvironment();
+    Object result = env.function("let x = 1\nreturn x + 2");
+
+    assertThat(result, instanceOf(MethodHandle.class));
+
+    MethodHandle func = (MethodHandle) result;
+    assertThat((Integer) func.invoke(), is(3));
+  }
+
+  @Test
+  public void run() throws Throwable {
+    EvaluationEnvironment env = new EvaluationEnvironment();
+    Object result = env.run("let x = 1\nreturn x + 2");
+
+    assertThat(result, instanceOf(Integer.class));
+    assertThat((Integer) result, is(3));
+  }
+}
