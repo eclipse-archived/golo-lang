d9820882d3a111e7c8e82ed10c294c238719f97b
Julien Ponge <julien.ponge@insa-lyon.fr>
2013-06-11 23:11:36 +0200

    EvaluationEnvironment javadoc

--- src/main/java/gololang/EvaluationEnvironment.java
+++ src/main/java/gololang/EvaluationEnvironment.java
@@ -24,6 +24,37 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
+/**
+ * An evaluation environment offers facilities for dynamic code compilation, loading and execution from Golo code as
+ * strings.
+ * <p>
+ * An evaluation environment is reusable across several executions. The only exception is when using {@code asModule()},
+ * as attempts to load a module with the same name as an already loaded one fails.
+ * <p>
+ * Each instance of this class uses a dedicated {@link GoloClassLoader}, hence usual rules about classloader delegation
+ * and isolation apply to evaluation environments.
+ * <p>
+ * While dynamic code evaluation is useful, it shall still be used with care and parsimony. It is especially important
+ * not to abuse {@code run()}, as each invocation triggers the generation of a one-shot class.
+ * <p>
+ * Here is an example usage of this API:
+ * <pre>
+ * let env = EvaluationEnvironment()
+ * let code =
+ * """
+ * function a = -> "a."
+ * function b = -> "b."
+ * """
+ * let mod = env: anonymousModule(code)
+ * let a = fun("a", mod)
+ * let b = fun("b", mod)
+ * println(a())
+ * println(b())
+ * </pre>
+ * <p>
+ * While this class is expected to be used from Golo code, it can also be used as a convenient way to embed Golo into
+ * polyglot JVM applications.
+ */
 public class EvaluationEnvironment {
 
   private final GoloClassLoader goloClassLoader = new GoloClassLoader();
@@ -37,17 +68,57 @@ public class EvaluationEnvironment {
     return "module anonymous" + System.nanoTime();
   }
 
+  /**
+   * Imports symbols.
+   * <p>
+   * Each symbol generates an equivalent {@code import} statement in the corresponding Golo code. Calling
+   * {@code imports("foo.Bar", "bar.Baz")} means that the subsequent code evaluations have {@code import foo.Bar} and
+   * {@code import bar.Baz} statements.
+   * <p>
+   * Note that this has no effect for {@link #asModule(String)}. Also, calling this method several times accumulates
+   * the imports, in order.
+   *
+   * @param head the first imported symbol.
+   * @param tail the next imported symbols.
+   * @return this evaluation environment.
+   */
   public EvaluationEnvironment imports(String head, String... tail) {
     imports.add(head);
     Collections.addAll(imports, tail);
     return this;
   }
 
+  /**
+   * Clears all import symbols for the next code evaluation requests.
+   *
+   * @return this evaluation environment.
+   */
   public EvaluationEnvironment clearImports() {
     imports.clear();
     return this;
   }
 
+  /**
+   * Evaluates a complete module, as in:
+   * <pre>
+   * let code =
+   * """
+   * module foo
+   *
+   * function a = -> "a!"
+   * function b = -> "b!"
+   * """
+   * let mod = env: asModule(code)
+   * let a = fun("a", mod)
+   * let b = fun("b", mod)
+   * println(a())
+   * println(b())
+   * </pre>
+   *
+   * @param source the module Golo source code as a string.
+   * @return the corresponding module, as a {@link Class}.
+   * @see Predefined#fun(Object, Object)
+   */
   public Object asModule(String source) {
     try (InputStream in = new ByteArrayInputStream(source.getBytes())) {
       return goloClassLoader.load(anonymousFilename(), in);
@@ -56,22 +127,102 @@ public class EvaluationEnvironment {
     }
   }
 
+  /**
+   * Loads an anonymous module. This is the same as {@link #asModule(String)}, except that the code does not contain
+   * a {@code module} declaration.
+   *
+   * <pre>
+   * let code =
+   * """
+   * function a = -> "a!"
+   * function b = -> "b!"
+   * """
+   * let mod = env: anonymousModule(code)
+   * let a = fun("a", mod)
+   * let b = fun("b", mod)
+   * println(a())
+   * println(b())
+   * </pre>
+   *
+   * @param source the module Golo source code as a string.
+   * @return the corresponding module, as a {@link Class}.
+   * @see Predefined#fun(Object, Object)
+   */
   public Object anonymousModule(String source) {
     return asModule(anonymousModuleName() + "\n\n" + source);
   }
 
+  /**
+   * Defines a function, and returns it.
+   *
+   * <pre>
+   * let code = "|a, b| -> (a + b) * 2"
+   * let f = env: def(code)
+   * println(f(10, 20))
+   * </pre>
+   *
+   * @param source the function code.
+   * @return the function as a {@link java.lang.invoke.MethodHandle} instance.
+   */
   public Object def(String source) {
     return loadAndRun("return " + source, "$_code");
   }
 
+  /**
+   * Evaluates some code as the body of a function and returns it.
+   *
+   * <pre>
+   * let code = "return (a + b) * 2"
+   * let f = env: asFunction(code, "a", "b")
+   * println(f(10, 20))
+   * </pre>
+   *
+   * @param source        the function body source code.
+   * @param argumentNames the argument names.
+   * @return the function as a {@link java.lang.invoke.MethodHandle} instance.
+   */
   public Object asFunction(String source, String... argumentNames) {
     return loadAndRun(source, "$_code_ref", argumentNames);
   }
 
+  /**
+   * Runs some code as the body of a function and returns the value. The code shall use {@code return} statements
+   * to provide return values, if any.
+   *
+   * <pre>
+   * let code = """println(">>> run")
+   * foreach (i in range(0, 3)) {
+   *   println("w00t")
+   * }
+   * return 666"""
+   * env: run(code)
+   *
+   * </pre>
+   *
+   * @param source the source to run.
+   * @return the return value, or {@code null} if no {@code return} statement is used.
+   */
   public Object run(String source) {
     return loadAndRun(source, "$_code");
   }
 
+  /**
+   * Runs some code as the body of a function and returns the value. This is the same as {@link #run(String)}, but it
+   * takes a set of reference bindings in a map. Each reference is equivalent to a {@code let} statement.
+   *
+   * <pre>
+   * let code = """println(">>> run_map")
+   * println(a)
+   * println(b)
+   * """
+   * let values = java.util.TreeMap(): add("a", 1): add("b", 2)
+   * env: run(code, values)
+   * </pre>
+   *
+   * @param source  the source to run.
+   * @param context a map of bindings from name to values.
+   * @return the return value, or {@code null} if no {@code return} statement is used.
+   */
   public Object run(String source, Map<String, Object> context) {
     StringBuilder builder = new StringBuilder();
     for (String param : context.keySet()) {
