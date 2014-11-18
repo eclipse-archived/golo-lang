e468e9db0b1d2b30683e5a2240b2b0a2b4702b56
Yannick Loiseau <me@yloiseau.net>
2014-11-18 18:03:25 +0100

    Code formating

--- src/main/java/fr/insalyon/citi/golo/runtime/AmbiguousFunctionReferenceException.java
+++ src/main/java/fr/insalyon/citi/golo/runtime/AmbiguousFunctionReferenceException.java
@@ -17,12 +17,15 @@
 package fr.insalyon.citi.golo.runtime;
 
 public class AmbiguousFunctionReferenceException extends ReflectiveOperationException {
+
   public AmbiguousFunctionReferenceException(String message) {
     super(message);
   }
+
   public AmbiguousFunctionReferenceException(String message, Throwable cause) {
     super(message, cause);
   }
+
   public AmbiguousFunctionReferenceException(Throwable cause) {
     super(cause);
   }
