0c6c7c1c21a6c21cea258d98736969b1821a2baf
Yannick Loiseau <yannick.loiseau@univ-bpclermont.fr>
2015-08-13 10:40:25 +0200

    Documentation and feedback
    Adds documentation for the special union test methods, and integrates
    comments (Unknown class made final).

--- src/main/java/gololang/Unknown.java
+++ src/main/java/gololang/Unknown.java
@@ -5,8 +5,10 @@ package gololang;
  * A singleton class that is equals to any object.
  * <p>
  * Used in unions matching special methods to ignore a value.
+ * Note that this break the commutativity property of {@code equals}, and thus this object must be
+ * used with caution.
  */
-public class Unknown {
+public final class Unknown {
   private static final Unknown instance = new Unknown();
   private Unknown() {}
   public static Unknown get() { 
