04cd7807e419457b10f0e2103f793d2cf1c12452
Yannick Loiseau <yannick.loiseau@univ-bpclermont.fr>
2017-02-28 11:57:48 +0100

    The big yearly copyright update

--- src/main/java/gololang/GoloAdapter.java
+++ src/main/java/gololang/GoloAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2016 Institut National des Sciences Appliquées de Lyon (INSA-Lyon)
+ * Copyright (c) 2012-2017 Institut National des Sciences Appliquées de Lyon (INSA-Lyon)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
