ce29ca19b90a843afc5e6bc66cfed362aad3cdf7
Julien Ponge <julien.ponge@gmail.com>
2020-10-26 17:15:04 +0100

    Move back to snapshots

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 }
 
 group = "org.eclipse.golo"
-version = "3.4.0-M1"
+version = "3.4.0-SNAPSHOT"
 
 val generatedSourcesDir = "$projectDir/src/main/generated"
 val goloCliMain = "org.eclipse.golo.cli.Main"
